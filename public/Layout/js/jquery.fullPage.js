a54bfa652fda00319c860c9a08bc528e2149d9ea
smartendude <smartendude@gmail.com>
2015-10-23 11:49:50 +0700

    add logic to slide navigation now the label can be set by the name attribute on slide section in html

--- public/Layout/js/jquery.fullPage.js
+++ public/Layout/js/jquery.fullPage.js
@@ -2084,16 +2084,18 @@
         function addSlidesNavigation(section, numSlides){
             section.append('<div class="' + SLIDES_NAV + '"><ul></ul></div>');
             var nav = section.find(SLIDES_NAV_SEL);
+            var slides = section.find(SLIDES_WRAPPER_SEL);
 
             //top or bottom
             nav.addClass(options.slidesNavPosition);
 
             for(var i=0; i< numSlides; i++){
-                nav.find('ul').append('<li><a href="#"><span></span></a></li>');
+                var slide =  slides.find(SLIDE_SEL).eq(i);
+                nav.find('ul').append('<li><a href="#">' +slide.attr("name")+ '</a></li>');
             }
 
             //centering it
-            nav.css('margin-left', '-' + (nav.width()/2) + 'px');
+            nav.css('margin-left', '-' + numSlides*100 + 'px');
 
             nav.find('li').first().find('a').addClass(ACTIVE);
         }
