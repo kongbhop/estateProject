b963d20fae2c229b58c9432526bba7263dee3629
smartendude <smartendude@gmail.com>
2015-10-25 17:20:25 +0700

    change style in floor plan page

--- app/views/index.php
+++ app/views/index.php
@@ -647,15 +647,15 @@
 		<div class="slide planFloor" id="planAFloor" name="<?= Lang::get('content.floorPlan') ?>">
 			<div class="sideCover">
 				<div class="floorPlan">
-					<a class="fancybox" rel="fancybox" href="imgs/sec0.jpg" title="">
-						<img src="imgs/sec0.jpg" alt="" />
+					<a class="fancybox" rel="fancybox" href="imgs/A1.jpg" title="">
+						<img src="imgs/A1.jpg" alt="" />
 					</a>
 					<p class="textTopic"><?= Lang::get('content.planADownTopic') ?></p>
 					<div class="textLeft textDesp"><?= Lang::get('content.planADownContent') ?></div>
 				</div>
 				<div class="floorPlan">
-					<a class="fancybox" rel="fancybox" href="imgs/sec1-1.jpg" title="">
-						<img src="imgs/sec1-1.jpg" alt="" />
+					<a class="fancybox" rel="fancybox" href="imgs/A2.jpg" title="">
+						<img src="imgs/A2.jpg" alt="" />
 					</a>
 					<p class="textTopic"><?= Lang::get('content.planAUpTopic') ?></p>
 					<div class="textLeft textDesp"><?= Lang::get('content.planAUpContent') ?></div>
@@ -673,18 +673,18 @@
 		<div class="slide planFloor" id="planBFloor" name="<?= Lang::get('content.floorPlan') ?>">
 			<div class="sideCover">
 				<div class="floorPlan">
-					<a class="fancybox" rel="fancybox" href="imgs/sec0.jpg" title="">
-						<img src="imgs/sec0.jpg" alt="" />
+					<a class="fancybox" rel="fancybox" href="imgs/B1.jpg" title="">
+						<img src="imgs/B1.jpg" alt="" />
 					</a>
 					<p class="textTopic"><?= Lang::get('content.planBDownTopic') ?></p>
-					<div class="textLeft textDesp"><?= Lang::get('content.planBDownContent') ?></div>
+					<div class="textDesp"><?= Lang::get('content.planBDownContent') ?></div>
 				</div>
 				<div class="floorPlan">
-					<a class="fancybox" rel="fancybox" href="imgs/sec1-1.jpg" title="">
-						<img src="imgs/sec1-1.jpg" alt="" />
+					<a class="fancybox" rel="fancybox" href="imgs/B2.jpg" title="">
+						<img src="imgs/B2.jpg" alt="" />
 					</a>
 					<p class="textTopic"><?= Lang::get('content.planBUpTopic') ?></p>
-					<div class="textLeft textDesp"><?= Lang::get('content.planBUpContent') ?></div>
+					<div class="textDesp"><?= Lang::get('content.planBUpContent') ?></div>
 				</div>
 			</div>
 		</div>
