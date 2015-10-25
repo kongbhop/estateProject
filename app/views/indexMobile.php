a2d1687bc668ce90fafe8c41d2d7f03ea8b3718a
Pat <pat.hornchaiya@hotmail.com>
2015-10-25 17:58:35 +0700

    Add files required for mobile page

--- app/views/indexMobile.php
+++ app/views/indexMobile.php
@@ -1,1 +1,24 @@
-<h1>HELLO</h1>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  	<title>Private Park 3 Project</title>
+  	<meta name="author" content="" />
+  	<meta name="description" content="" />
+  	<meta name="keywords"  content="" />
+  	<meta name="Resource-type" content="Document" />
+
+    <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+    <script src="semantic/dist/semantic.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/styleMobileM.css">
+    <link rel="stylesheet" type="text/css" href="css/styleMobileC.css">
+
+  </head>
+  <body>
+
+
+
+  </body>
+</html>
