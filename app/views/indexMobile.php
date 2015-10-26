143cd1ab4f46b255a368dce8d93c1a44e4977895
Pat <pat.hornchaiya@hotmail.com>
2015-10-26 22:41:45 +0700

    Add logic to sidebar

--- app/views/indexMobile.php
+++ app/views/indexMobile.php
@@ -14,7 +14,7 @@
 
 
   <link rel="stylesheet" type="text/css" href="css/jquery.fullPage.css" />
-  
+
   <link rel="stylesheet" type="text/css" href="css/styleMobileM.css">
   <link rel="stylesheet" type="text/css" href="css/styleMobileC.css">
 
@@ -29,10 +29,13 @@
   <script src="https://maps.googleapis.com/maps/api/js"></script>
 
   <script type="text/javascript" src="js/jquery.fullPage.js"></script>
-  <script type="text/javascript" src="js/script.js"></script>
+  <script type="text/javascript" src="js/scriptMobile.js"></script>
 
   <link rel="stylesheet" href="css/jquery.fancybox.css" type="text/css" media="screen" />
   <script type="text/javascript" src="js/jquery.fancybox.pack.js"></script>
+
+  <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" media="screen" />
+  <script type="text/javascript" src="js/snap.min.js"></script>
 
 
   <script type="text/javascript">
@@ -54,8 +57,35 @@
 </head>
 <body>
 
+<div class="snap-drawers">
+    <div class="snap-drawer snap-drawer-right">
+        <div>
+            <ul>
+              <li data-menuanchor="Project"><a href="#Project"><?= Lang::get('content.navProject') ?></a></li>
+              <li data-menuanchor="Location"><a href="#Location"><?= Lang::get('content.navLocation') ?></a></li>
+              <li data-menuanchor="PlanA"><a href="#PlanA"><?= Lang::get('content.navPlanA') ?></a></li>
+              <li data-menuanchor="PlanB"><a href="#PlanB"><?= Lang::get('content.navPlanB') ?></a></li>
+              <li data-menuanchor="Samapan"><a href="#Samapan"><?= Lang::get('content.navSamapan') ?></a></li>
+              <li data-menuanchor="Contact"><a href="#Contact"><?= Lang::get('content.navContact') ?></a></li>
+              <li>
+                <span class="<?= (Session::get('lang') == 'th')? 'active':'' ?>"><a href="th"><img src="imgs/th_icon.png" alt=""> ไทย</a></span>
+                 |
+                <span class="<?= (Session::get('lang') == 'en')? 'active':'' ?>"><a href="en"><img src="imgs/us_icon.png" alt=""> ENGLISH</a></span>
+              </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="overlay"></div>
 
 
+<div id="toolbar">
+  <div class="logo">
+    <a href="#Mainpage"><img src="imgs/logo.png" /></a>
+    <a href="#" id="open-right"><i class="fa fa-bars"></i></a>
+    <a href="#" id="open-location"><i class="fa fa-map-marker"></i></a>
+  </div>
+</div>
 <div id="fullpage">
   <div class="section" id="mainpage">
       <div class="centerText centerDiv" id="mainpageText">
@@ -101,7 +131,7 @@
 
   </div>
 
-  
+
 
 </div>
 
